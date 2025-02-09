fbb820c24ce397ebd8065dc83541cd3b5ed1ef5a
jannis <jannis.austgen@gmail.com>
2025-02-09 01:38:47 +0100

    feat: - added comment to Toolbar component

--- nabp/src/components/Toolbar/Toolbar.tsx
+++ nabp/src/components/Toolbar/Toolbar.tsx
@@ -11,6 +11,15 @@ interface ToolbarProps {
     backButton: boolean;
 }
 
+/**
+ * Toolbar component
+ * @param {boolean} searchable - boolean to determine if search input should be displayed
+ * @param {string} pageTitle - string to display as page title
+ * @param {boolean} backButton - boolean to determine if back button should be displayed
+ * @returns {JSX.Element}
+ * This component is used to display the toolbar at the top of the page.
+ * the toolbar should be used in every page with various states.
+ **/
 const Toolbar: React.FC<ToolbarProps> = ({ searchable, pageTitle, backButton }) => {
     const [searchableState, setSearchableState] = useState(searchable);
     const [pageTitleState, setPageTitleState] = useState(pageTitle);
@@ -25,7 +34,7 @@ const Toolbar: React.FC<ToolbarProps> = ({ searchable, pageTitle, backButton }) 
             </div>
             <div>
                 <p style={{
-                    textAlign: "right",                 //kann ganz ersetzt werden
+                    textAlign: "right",                 //TODO kann ganz ersetzt werden
                     color: "white",
                 }}>Placeholder für navigator</p>
             </div>
