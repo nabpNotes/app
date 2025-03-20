cdec4c14e772b2bb0d78c04326bf6539f37acffb
Jannis Austgen <jannis.austgen@gmail.com>
2025-03-20 21:49:37 +0100

    feat: -added basic swipe functions

--- src/components/ListItem/TextItem/TextItem.tsx
+++ src/components/ListItem/TextItem/TextItem.tsx
@@ -1,5 +1,7 @@
 import styles from './TextItem.module.css';
-import React, {useState} from "react";
+import React, {useEffect, useRef, useState} from "react";
+import {IonIcon, IonItem, IonItemOption, IonItemOptions, IonItemSliding} from "@ionic/react";
+import { trash, settings } from 'ionicons/icons'; //TODO: Change Icon
 
 interface TextItemProps {
     itemData: {
@@ -10,12 +12,31 @@ interface TextItemProps {
 
 const TextItem: React.FC<TextItemProps> = ({itemData}: TextItemProps): JSX.Element => {
     const [itemDataState, setItemDataState] = useState(itemData);
+    const slidingRef = useRef<HTMLIonItemSlidingElement>(null);
+
+    const editItem = () => {
+        console.log('Edit clicked');
+    }
 
     return (
-        <div className={styles.textItem}>
-            <h2>{itemDataState.title}</h2>
-            <p>{itemDataState.text}</p>
-        </div>
+     <IonItemSliding className={styles.textItem} ref={slidingRef}>
+         <IonItemOptions side="start" onIonSwipe={() => {slidingRef.current?.close(); editItem()}}>
+             <IonItemOption color="primary">
+                 <IonIcon slot="start" icon={settings}></IonIcon>
+             </IonItemOption>
+         </IonItemOptions>
+         <IonItem className={styles.ionItem}>
+             <div>
+                 <h2>{itemDataState.title}</h2>
+                 <p>{itemDataState.text}</p>
+             </div>
+         </IonItem>
+         <IonItemOptions side="end">
+             <IonItemOption color="danger" onClick={() => console.log('Delete clicked')}>
+                 <IonIcon slot="icon-only" icon={trash}></IonIcon>
+             </IonItemOption>
+         </IonItemOptions>
+     </IonItemSliding>
     );
 }
 
